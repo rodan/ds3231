b1a555a04e14161e4983a1c5dcb58fdf89f4d5f2
storca <geek392662@gmail.com>
2018-07-04 19:26:41 +0200

    Reorganised library with a class, supports the older version

---
+++ examples/rtc_ds3231_newlib/rtc_ds3231_newlib.ino
@@ -0,0 +1,147 @@
+
+#include <Wire.h>
+//Use the new library
+#define DS3231_NEWLIB
+#include "ds3231.h"
+
+#define BUFF_MAX 128
+
+uint8_t time[8];
+char recv[BUFF_MAX];
+unsigned int recv_size = 0;
+unsigned long prev, interval = 5000;
+
+//Create an object
+DS3231 rtc();
+
+void parse_cmd(char *cmd, int cmdsize);
+
+void setup()
+{
+    Serial.begin(9600);
+    Wire.begin();
+    rtc.begin();
+    memset(recv, 0, BUFF_MAX);
+    Serial.println("GET time");
+}
+
+void loop()
+{
+    char in;
+    char buff[BUFF_MAX];
+    unsigned long now = millis();
+    struct ts t;
+
+    // show time once in a while
+    if ((now - prev > interval) && (Serial.available() <= 0)) {
+        rtc.get(&t);
+
+        // there is a compile time option in the library to include unixtime support
+#ifdef CONFIG_UNIXTIME
+        snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d %ld", t.year,
+             t.mon, t.mday, t.hour, t.min, t.sec, t.unixtime);
+#else
+        snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", t.year,
+             t.mon, t.mday, t.hour, t.min, t.sec);
+#endif
+
+        Serial.println(buff);
+        prev = now;
+    }
+
+    if (Serial.available() > 0) {
+        in = Serial.read();
+
+        if ((in == 10 || in == 13) && (recv_size > 0)) {
+            parse_cmd(recv, recv_size);
+            recv_size = 0;
+            recv[0] = 0;
+        } else if (in < 48 || in > 122) {;       // ignore ~[0-9A-Za-z]
+        } else if (recv_size > BUFF_MAX - 2) {   // drop lines that are too long
+            // drop
+            recv_size = 0;
+            recv[0] = 0;
+        } else if (recv_size < BUFF_MAX - 2) {
+            recv[recv_size] = in;
+            recv[recv_size + 1] = 0;
+            recv_size += 1;
+        }
+
+    }
+}
+
+void parse_cmd(char *cmd, int cmdsize)
+{
+    uint8_t i;
+    uint8_t reg_val;
+    char buff[BUFF_MAX];
+    struct ts t;
+
+    //snprintf(buff, BUFF_MAX, "cmd was '%s' %d\n", cmd, cmdsize);
+    //Serial.print(buff);
+
+    // TssmmhhWDDMMYYYY aka set time
+    if (cmd[0] == 84 && cmdsize == 16) {
+        //T355720619112011
+        t.sec = inp2toi(cmd, 1);
+        t.min = inp2toi(cmd, 3);
+        t.hour = inp2toi(cmd, 5);
+        t.wday = cmd[7] - 48;
+        t.mday = inp2toi(cmd, 8);
+        t.mon = inp2toi(cmd, 10);
+        t.year = inp2toi(cmd, 12) * 100 + inp2toi(cmd, 14);
+        rtc.set(t);
+        Serial.println("OK");
+    } else if (cmd[0] == 49 && cmdsize == 1) {  // "1" get alarm 1
+        rtc.get_a1(&buff[0], 59);
+        Serial.println(buff);
+    } else if (cmd[0] == 50 && cmdsize == 1) {  // "2" get alarm 1
+        rtc.get_a2(&buff[0], 59);
+        Serial.println(buff);
+    } else if (cmd[0] == 51 && cmdsize == 1) {  // "3" get aging register
+        Serial.print("aging reg is ");
+        Serial.println(rtc.get_aging(), DEC);
+    } else if (cmd[0] == 65 && cmdsize == 9) {  // "A" set alarm 1
+        rtc.set_creg(DS3231_INTCN | DS3231_A1IE);
+        //ASSMMHHDD
+        for (i = 0; i < 4; i++) {
+            time[i] = (cmd[2 * i + 1] - 48) * 10 + cmd[2 * i + 2] - 48; // ss, mm, hh, dd
+        }
+        uint8_t flags[5] = { 0, 0, 0, 0, 0 };
+        rtc.set_a1(time[0], time[1], time[2], time[3], flags);
+        rtc.get_a1(&buff[0], 59);
+        Serial.println(buff);
+    } else if (cmd[0] == 66 && cmdsize == 7) {  // "B" Set Alarm 2
+        rtc.set_creg(DS3231_INTCN | DS3231_A2IE);
+        //BMMHHDD
+        for (i = 0; i < 4; i++) {
+            time[i] = (cmd[2 * i + 1] - 48) * 10 + cmd[2 * i + 2] - 48; // mm, hh, dd
+        }
+        uint8_t flags[5] = { 0, 0, 0, 0 };
+        rtc.set_a2(time[0], time[1], time[2], flags);
+        rtc.get_a2(&buff[0], 59);
+        Serial.println(buff);
+    } else if (cmd[0] == 67 && cmdsize == 1) {  // "C" - get temperature register
+        Serial.print("temperature reg is ");
+        Serial.println(rtc.get_treg(), DEC);
+    } else if (cmd[0] == 68 && cmdsize == 1) {  // "D" - reset status register alarm flags
+        reg_val = rtc.get_sreg();
+        reg_val &= B11111100;
+        rtc.set_sreg(reg_val);
+    } else if (cmd[0] == 70 && cmdsize == 1) {  // "F" - custom fct
+        reg_val = rtc.get_addr(0x5);
+        Serial.print("orig ");
+        Serial.print(reg_val,DEC);
+        Serial.print("month is ");
+        Serial.println(bcdtodec(reg_val & 0x1F),DEC);
+    } else if (cmd[0] == 71 && cmdsize == 1) {  // "G" - set aging status register
+        rtc.set_aging(0);
+    } else if (cmd[0] == 83 && cmdsize == 1) {  // "S" - get status register
+        Serial.print("status reg is ");
+        Serial.println(rtc.get_sreg(), DEC);
+    } else {
+        Serial.print("unknown command prefix ");
+        Serial.println(cmd[0]);
+        Serial.println(cmd[0], DEC);
+    }
+}
